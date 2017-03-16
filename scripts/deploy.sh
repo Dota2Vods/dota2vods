04b952aebf665681d053a24e364f3e05880d89ce
zyberspace <zyberspace@zyberware.org>
2017-03-16 13:14:01 +0100

    Fixed mime type

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@ echo "Making sure s3cmd is installed..."
 pip install --user --quiet s3cmd
 echo "Syncing build folder with s3 bucket..."
 ~/.local/bin/s3cmd sync --reduced-redundancy --acl-public --delete-removed \
-    --add-header="Cache-Control: public, max-age=31536000, no-cache" \
+    --no-mime-magic --guess-mime-type --add-header="Cache-Control: public, max-age=31536000, no-cache" \
     build/ s3://dota2vods/
 echo "Done!"
