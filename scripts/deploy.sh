dfb4370d29dc48e6027bbea791ae3c62dc3544bd
zyberspace <zyberspace@zyberware.org>
2017-03-17 10:11:54 +0100

    Don't upload file system informations to s3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 echo "Making sure s3cmd is installed..."
 pip install --user --quiet s3cmd
 echo "Syncing build folder with s3 bucket..."
-~/.local/bin/s3cmd sync --reduced-redundancy --acl-public --delete-removed \
+~/.local/bin/s3cmd sync --reduced-redundancy --acl-public --delete-removed --no-preserve \
     --no-mime-magic --guess-mime-type --add-header="Cache-Control: public, max-age=31536000, no-cache" \
     build/ s3://dota2vods/
 echo "Done!"
