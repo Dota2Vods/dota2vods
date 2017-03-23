7ff51c82bd30002a1b10162cc0d710ef68dec985
zyberspace <zyberspace@zyberware.org>
2017-03-23 21:05:14 +0100

    json-store: FileProvider caches the file system io

--- src/json-store/FileProvider.js
+++ src/json-store/FileProvider.js
@@ -2,13 +2,23 @@ import path from "path";
 import { readFileSync } from "fs";
 import AbstractProvider from "./AbstractProvider";
 
+//Process wide fs cache (The json files wont change during a build)
+let jsonCache = {};
+function parseJSONFile(filePath) {
+    if (!jsonCache[filePath]) {
+        jsonCache[filePath] = JSON.parse(readFileSync(filePath));
+    }
+
+    return jsonCache[filePath];
+}
+
 export default class FileProvider extends AbstractProvider {
     get(jsonFile) {
         const dataFolder = this.props.dataFolder;
         const filePath = path.join(dataFolder, jsonFile.replace(".", "")) + ".json";
 
         try {
-            return JSON.parse(readFileSync(filePath));
+            return parseJSONFile(filePath);
         } catch (error) {
             return false;
         }
